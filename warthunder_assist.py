e7900971b22f692117765637d4936bf093a1ce75
Apollo Burton <124854003+Apollo-Burton@users.noreply.github.com>
2023-02-23 02:05:26 -0800

    Update warthunder_assist.py

--- warthunder_assist.py
+++ warthunder_assist.py
@@ -1,17 +1,10 @@
-import pyautogui as pag
-from playsound import playsound
-import time
-import math
-
-time.sleep(1)
-
 # =========================================================================================================================================================================
 # USER CONFIGURATION
 
 # =========================================================================================================================================================================
 # IMPORTANT
 
-sound_path = "C:\\Users\\User\\Downloads\\mixkit-alarm-tone-996.wav"
+sound_path = "C:\\Users\\User\\Desktop\\Programs\\mixkit-alarm-tone-996.wav"
 
 # "sound_path" is telling the program where the sound file is on your computer. If you installed "mixkit-alarm-tone-996.wav" file from github, "sound_path" should be 
 # "C:\\Users\\User\\Downloads\\mixkit-alarm-tone-996.wav". "User" SHOULD BE YOUR ACCOUNT NAME ON WINDOWS, if not, it won't work. If you want to use your own audio file,
@@ -21,13 +14,13 @@ sound_path = "C:\\Users\\User\\Downloads\\mixkit-alarm-tone-996.wav"
 # Make sure to double the backslashes: (C:\Users\User\Downloads\file.mp3 -> C:\\Users\\User\\Downloads\\file.mp3)
 # =========================================================================================================================================================================
 
-top_left = (1730, 125)
+top_left = (1735, 125)
 bottom_right = (1850, 190)
 center_of_circle = (1785, 125)
 radius_of_circle = 65
 
-color_in_rgb = (190, 190, 30)
-color_range = 20
+color_in_rgb = (185, 185, 60)
+color_range = 30
 timer = 1
 
 # "top_left" and "bottom_right" are the top left and bottom right corners of the box the program will search in x and y coordinates. The "radius_of_circle" and "center_of_circle" 
@@ -39,11 +32,18 @@ timer = 1
 # Change "color_range" to change the accuracy. If it is set to "0", the program will only look for the exact value of "color_in_rgb". If it is set higher, it will look for colors
 # similar to "color_in_rgb". this is helpful when the color you are looking for is part of a gradient or if it slightly changes over time. The higher "color_range" is set, the
 # less accurate the program will be. For example, if "color_in_rgb" is (50, 50, 50), and "color_range" is 50, then any color from (0, 0, 0) to (99, 99, 99) will be detected.
-# I recommend setting "color_range" to 20 or less, as setting it too high will make the program very innacurate.
+# I recommend setting "color_range" to 30 or less, as setting it too high will make the program innacurate.
 
 # "timer" controls how long you want the program to run in minutes.
+# To see debug info (location of pixels and what the rgb value of the pixels are), go to line 62 and remove the hashtag
 # =========================================================================================================================================================================
 
+import pyautogui as pag
+from playsound import playsound
+import time
+import math
+
+time.sleep(1)
 
 def get_colors(color: tuple[int, int, int], rgb_range: int, center: tuple[int, int], radius: int):
     screen = pag.screenshot()
@@ -58,6 +58,7 @@ def get_colors(color: tuple[int, int, int], rgb_range: int, center: tuple[int, i
             if distance <= radius:
                 # Checking if the pixel is in the color_range
                 rgb = screen.getpixel((x, y))
+                #print([x, y], rgb)
                 if rgb[0] >= color[0] - rgb_range and rgb[0] <= color[0] + rgb_range and rgb[1] >= color[1] - rgb_range and rgb[1] <= color[1] + rgb_range and rgb[2] >= color[2] - rgb_range and rgb[2] <= color[2] + rgb_range:
                     playsound(sound_path)
                     return True
